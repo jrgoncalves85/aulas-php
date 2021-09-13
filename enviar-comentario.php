a12352b2daddba7b3dbf4c5bf53bcd23114adc4a
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-09-13 16:05:41 -0300

    adicionado conexão com o banco para gravar comentários

--- enviar-comentario.php
+++ enviar-comentario.php
@@ -5,13 +5,16 @@ $turma = $_POST['optturma'];
 $comentario = $_POST['txtcomentario'];
 
 echo "<h1>Comentário</h1>";
-echo "<p>Nome: $nome";
-echo "<p>Turma: $turma";
-echo "<p>Comentario: $comentario";
-echo "<br>";
+echo "<p>Nome: $nome </p>";
+echo "<p>Turma: $turma </p>";
+echo "<p>Comentário: $comentario </p>";
 
-$sql = "insert into comentarios (nome, turma, comentario) values ('$nome','$turma','$comentario')";
+$sql = "insert into comentarios (nome, turma, comentario)
+            values ('$nome' , '$turma', '$comentario') ";
 
-echo $sql;
+$conexao = new PDO('mysql:host=127.0.0.1;dbname=pessoal', 'root', '');
+$conexao->exec($sql);
+
+echo "Comentário enviado com sucesso!";
 
 ?>
