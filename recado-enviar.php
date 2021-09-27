b47948045c14055149f0b5b0803cd0e0a46cee0e
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-09-27 20:18:50 -0300

    alterado nomenclatura de comentários para recados

--- recado-enviar.php
+++ recado-enviar.php
@@ -2,23 +2,23 @@
 
 $nome = $_POST['txtnome'];
 $turma = $_POST['optturma'];
-$comentario = $_POST['txtcomentario'];
+$recado = $_POST['txtrecado'];
 
 echo "<h1>Comentário</h1>";
 echo "<p>Nome: $nome </p>";
 echo "<p>Turma: $turma </p>";
-echo "<p>Comentário: $comentario </p>";
+echo "<p>Recado: $recado </p>";
 
-$sql = "insert into comentarios
-            (nome, turma, comentario)
+$sql = "insert into recados
+            (nome, turma, recado)
         values
-            ('$nome', '$turma', '$comentario')";
+            ('$nome', '$turma', '$recado')";
 
 echo $sql;
 
 $conexao = new PDO('mysql:host=127.0.0.1;dbname=pessoal','root','');
 $conexao->exec($sql);
 
-echo "<h2>Comentário gravado com sucesso</h2>";
+echo "<h2>Recado gravado com sucesso</h2>";
 
 ?>
